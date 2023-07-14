209fc686c5bca397473f379dcb31fbc3fb3db539
xiaoDeng <524846102@qq.com>
2023-07-14 18:29:56 +0800

    first commit

---
+++ sg-blog-vue/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="footer">
+        <p>鄂ICP备2023011111号-1 Copyright © HuaXian</p>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+  text-align: center;
+  background-color: #f5f5f5;
+  padding: 20px;
+}
+
+.footer p {
+  color: #999;
+  font-size: 14px;
+}
+</style>
